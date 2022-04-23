5afa8b1fad6b6ec39152ceeb40b0c931d60e3848
gretosk <101219020+gretosk@users.noreply.github.com>
2022-04-23 09:30:38 +0000

    Initial commit

---
+++ lib/logic/game.dart
@@ -0,0 +1,28 @@
+import 'dart:core';
+
+
+class Game {
+  static final boardLength = 9;
+
+  //TODO:
+  // - Player
+  // - Game
+  //  - variables - whos turn it is?
+  //  - board
+  //  - winnerCheck()
+
+
+  List<String> row1 = ['a1','b1','c1'];
+  List<String> row2 = ['a2','b2','c2'];
+  List<String> row3 = ['a3','b3','c3'];
+  List<String> col1 = ['a1','a2','a3'];
+  List<String> col2 = ['b1','b2','b3'];
+  List<String> col3 = ['c1','c2','c3'];
+  List<String> x1 = ['a1','b2','c3'];
+  List<String> x2 = ['c1','b2','a3'];
+
+
+
+
+
+}
